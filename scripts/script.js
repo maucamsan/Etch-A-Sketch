066855380406942e047cf5f1deef478e170881ec
Mauricio <maucamsan@gmail.com>
2022-07-12 16:38:46 -0500

    Refactor grid generation code
    When appending an element to a grid, the latter would know (since it has
    a grid template for columns and rows stablished) where to accomodate the
    element, so some lines of code were removed obtaining the same
    functionality.

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,18 @@
 const container = document.querySelector('#container');
 // Grid base of 16x16
 let size = 16;
+let cssTextGrid = `grid-template-columns:repeat(${size}, 1fr [col-start]);\n grid-template-rows:repeat(${size}, 1fr [row-start])`
 // Add grid to container
+container.style.cssText = cssTextGrid;
 
 for (let i = 0; i < size; i++){
-    let div = document.createElement('div');
-    div.style.cssText = `grid-column:${i+1}/${size}`
-    div.style.cssText = `grid-row:${i+1}/${size}`;
-    
-    div.innerText = 'x';
-    container.append(div);
+    for (let j = 0; j < size; j++){
+        let ndiv = document.createElement('div');
+        
+        container.append(ndiv);
+        // ndiv.style.cssText = `grid-column-start: column-start ${j}; grid-row-start: row-start ${i + j};`;
+        ndiv.innerText = 'x';
+        ndiv.style.cssText = 'display: inline-block; border: solid black 1px;';
+        
+    }
 }
-
-container.style.cssText = 'grid-template-columns:repeat(16,1fr)';
-container.style.cssText = 'grid-template-rows:repeat(16,1fr)';
